3d30c0d7bf6cdcc6c89ce7103879bf9164956221
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-12 22:32:08 +0100

    inject firestore

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@angular/core';
-import { Note } from '../interfaces/note.interface'
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
+  firestore: Firestore = inject(Firestore);
 
   constructor() { }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
